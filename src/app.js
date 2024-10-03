b0b5efb266ade623fed4ea9eafbdd8066ad777a3
Apoorva Kumar .P <bpapoorva@gmail.com>
2024-10-03 21:37:27 +0530

    Testing app.use

--- src/app.js
+++ src/app.js
@@ -7,6 +7,27 @@ app.listen(3000, (req, res) => {
 })
 
 
-app.get('/test', (req, res) => {
-  res.send(req.path)
+
+// if we use app.use -> handles all get put post -> and 1st route which matches the path will execute,others will not override
+// any route starts with /test will be matched here
+
+app.use('/test', (req, res) => {
+  res.send('test')
+})
+
+app.use('/test/123', (req, res) => {
+  res.send('test/123')
+})
+
+app.use('/test/123/1', (req, res) => {
+  res.send('test/123/1')
+})
+
+
+app.get('ab?c', (req, res) => {
+  console.log('b is optional here');
+})
+
+app.get('ab+c', (req, res) => {
+  console.log('we can have any number of b here');
 })
